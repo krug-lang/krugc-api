563009442f1a9257ec70ad595a4d74f52ad63ae4
Felix Angell <mail@felixangell.com>
2019-10-27 17:58:53 +0000

    mutable statement parse test

--- front/parse_test.go
+++ front/parse_test.go
@@ -5,6 +5,24 @@ import (
 	"testing"
 )
 
+func TestMutStatementParses(t *testing.T) {
+	input := []Token {
+		tok("mut", Identifier),
+		tok("x", Identifier),
+		tok("=", Symbol),
+		tok("5", Number),
+		tok(";", Symbol),
+	}
+	nodes, errs := ParseTokenStream(input)
+
+	mutStat := nodes[0].MutableStatementNode
+	assert.NotNil(t, mutStat)
+	assert.Equal(t, "x", mutStat.Name.Value)
+
+	assert.NotEmpty(t, nodes)
+	assert.Empty(t, errs)
+}
+
 func TestLetStatementParses(t *testing.T) {
 	input := []Token {
 		tok("let", Identifier),
